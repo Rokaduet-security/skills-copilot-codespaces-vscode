667a89d9b3244a3286421119be40a6758e0c2559
Rokaduet-security <rrrohitraj0@gmail.com>
2024-05-23 11:46:44 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,50 @@
+// Create web 
+// </p>
+//           </div>
+//         </div>
+//       </div>
+//       <div class="col-lg-4 col-md-6 col-sm-6">
+//         <div class="card">
+//           <div class="card-body">
+//             <h5 class="card-title">MongoDB</h5>
+//             <p class="card-text">MongoDB is a source-available cross-platform document-oriented database program.</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//   `;
+// }
+// function commentsMember() {
+//   // console.log("commentsMember");
+//   let member = document.getElementById("member");
+//   member.innerHTML = `
+//   <div class="container">
+//     <div class="row">
+//       <div class="col-lg-12">
+//         <h2 class="text-center">Comments</h2>
+//       </div>
+//     </div>
+//     <div class="row">
+//       <div class="col-lg-4 col-md-6 col-sm-6">
+//         <div class="card">
+//           <div class="card-body">
+//             <h5 class="card-title">HTML5</h5>
+//             <p class="card-text">HyperText Markup Language is the standard markup language for documents designed to be displayed in a web browser.</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div class="col-lg-4 col-md-6 col-sm-6">
+//         <div class="card">
+//           <div class="card-body">
+//             <h5 class="card-title">CSS3</h5>
+//             <p class="card-text">Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language like HTML.</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div class="col-lg-4 col-md-6 col-sm-6">
+//         <div class="card">
+//           <div class="card-body">
+//             <h5 class="card-title">JavaScript</h5>
+//             <p class="card-text">JavaScript is a high-level, interpreted programming language that conforms to the ECMAScript specification.</p>
+//
