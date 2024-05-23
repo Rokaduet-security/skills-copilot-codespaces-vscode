2cd73212c9c7eba25b1506cfe2b3c492af7c5c88
Rokaduet-security <rrrohitraj0@gmail.com>
2024-05-23 11:42:43 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,48 @@
+function skillsMember() {
+  // console.log("skillsMember");
+  let member = document.getElementById("member");
+  member.innerHTML = `
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center">Skills</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">HTML5</h5>
+            <p class="card-text">HyperText Markup Language is the standard markup language for documents designed to be displayed in a web browser.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">CSS3</h5>
+            <p class="card-text">Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language like HTML.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">JavaScript</h5>
+            <p class="card-text">JavaScript is a high-level, interpreted programming language that conforms to the ECMAScript specification.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">React</h5>
+            <p class="card-text">React is an open-source, front end, JavaScript library for building user interfaces or UI components.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Node.js</h5>
+            <p class="card-text">Node.js is an open-source,
